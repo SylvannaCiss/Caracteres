1b13a76d3c268c5dacd85e837a9f8371aa09e9bb
SylvannaCiss <20243ds146@utez.edu.mx>
2025-05-08 08:41:50 -0600

    Mi segundo programa en java

---
+++ Caracteres.java
@@ -0,0 +1,12 @@
+public class Caracteres {
+    public static void main(String[]args){
+        char letra = 'a';
+        char simbolo = '$';
+        char numeroChar = '5';
+
+        System.out.pritln("letra = " + letra);
+        System.out.println("simbolo = " + simbolo);
+        System.out.println("numeroChar = " + numeroChar);
+        
+    }
+}
